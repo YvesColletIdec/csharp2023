e90a4a8efee1630a2afb596616a7ebb1b47066b0
YvesColletIdec <85213449+YvesColletIdec@users.noreply.github.com>
2023-01-09 20:24:22 +0100

    mon premier projet c#

---
+++ MaPremiereSolution/MonPremierProjet/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.BackgroundColor = ConsoleColor.DarkBlue;
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.WriteLine(4.5);
+Console.Beep(300, 500);
+Console.ReadLine();
