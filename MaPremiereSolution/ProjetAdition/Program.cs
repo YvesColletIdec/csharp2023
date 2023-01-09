c7d2cc6ed399c38ae0dcb940ce7f6c006ce6855f
YvesColletIdec <85213449+YvesColletIdec@users.noreply.github.com>
2023-01-09 21:17:00 +0100

    ajout projet calcul somme

---
+++ MaPremiereSolution/ProjetAdition/Program.cs
@@ -0,0 +1,10 @@
+﻿//VAR
+int b = 0, a = 0;
+Console.WriteLine("Veuillez saisir le 1er nombre");
+//a = int.Parse(Console.ReadLine());
+a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Veuillez saisir le 2ème nombre");
+b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("la somme de " + a + " + " + b + " est de " + (a + b));
+Console.WriteLine($"la somme de {a} + {b} est de {(a + b)}");
+Console.ReadLine();
